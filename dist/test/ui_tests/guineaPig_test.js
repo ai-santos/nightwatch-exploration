add83f80e2a3a7b25ef499a888040ad86a3f587a
Aileen Santos <asantos3026@gmail.com>
2018-02-25 00:45:15 -0800

    Nightwatch setup

---
+++ dist/test/ui_tests/guineaPig_test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+require('../../../nightwatch.conf.js');
+
+module.exports = { // adapted from: https://git.io/vodU0
+  'Guinea Pig Assert Title': function GuineaPigAssertTitle(browser) {
+    browser.url('https://saucelabs.com/test/guinea-pig').waitForElementVisible('body').assert.title('I am a page title - Sauce Labs').saveScreenshot('guinea-pig-test.png').end();
+  }
+};
